9e27307ef1a45cb03f637d8d295c3fda21938700
michelleikeh <michelle.ikeh@gmail.com>
2023-01-04 13:42:20 -0800

    Task 0x01

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+def remove_char_at(str, n):
+    s = ""
+    for i in range(len(str)):
+        if i != n:
+            s += str[i]
+    return (s)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        num = number % 10
+    else:
+        num = number % -10
+        num *= -1
+    print(f"{num:d}")
+    return(num)
