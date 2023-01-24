2d511cb84215dc69c28c2e2285123c75358800b8
michelleikeh <michelle.ikeh@gmail.com>
2023-01-24 12:46:50 -0800

    Fix: 0-square.py error

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Module containing the square class """
-
+"""Module containing the square class"""
 class Square:
-    """An empty class called Square. """
+    """An empty class called Square."""
     pass
