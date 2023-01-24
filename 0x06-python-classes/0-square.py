48edc796ec505f62f7a31f72960d87936ed882bf
michelleikeh <michelle.ikeh@gmail.com>
2023-01-24 14:23:28 -0800

    Fix: style code error

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Module containing the square class"""
+"""Define a class Square."""
 
 class Square:
 
-    """An empty class called Square.
-    """
+    """Represent a square."""
     pass
