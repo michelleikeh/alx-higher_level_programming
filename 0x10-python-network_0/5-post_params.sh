aa0de343b62d3897f90dc4c73639cd71638cb7b4
michelle <michelle.ikeh@gmail.com>
2023-06-20 22:36:10 -0700

    fix:task 5 working

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-i#!/bin/bash
+#!/bin/bash
 # Script that sends a POST request and displays the body response
-curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"i
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
